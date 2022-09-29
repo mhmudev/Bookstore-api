cdebc02a92af39a98fea1acc979418706ede93ce
Mahmoud Attia <attiamahmoud222@gmail.com>
2022-09-29 23:42:31 +0200

    review schema and model

---
+++ models/Review.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      min: [1, "Rating minimum value is 1.0"],
+      max: [5, "Rating maximum value is 5.0"],
+      required: [true, "Review rating is required"],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Review user is required"],
+    },
+    book: {
+      type: mongoose.Types.ObjectId,
+      ref: "Book",
+      required: [true, "Review book is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Review", ReviewSchema);
